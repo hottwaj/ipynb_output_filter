642a50c5408317014e1ea53038a9f2e8a466ad94
hottwaj <jonathan.a.clarke@gmail.com>
2019-12-17 17:18:10 +0000

    added --infile --outfile and --ignore-errors options
    These help avoid using STDIN/STDOUT for git filter-branch operations, and allow skipping problematic/empty ipynb files

--- ipynb_output_filter.py
+++ ipynb_output_filter.py
@@ -10,6 +10,12 @@ try:
 except ValueError:
     pass
 
+try:
+    ignoreerrs_idx = sys.argv.index('--ignore-errors')
+    ignore_errs = True
+except ValueError:
+    ignore_errs = False
+    
 version = None
 
 if sys.version[0] == '2':
@@ -28,15 +34,44 @@ except ImportError:
         from IPython.nbformat.current import reads, write
         version = 'json'
 
-to_parse = sys.stdin.read()
+try:
+    infile_idx = sys.argv.index('--infile')
+    infilename = sys.argv[infile_idx+1]
+    import os
+    infilename = os.path.expanduser(infilename)
+    try:
+        with open(infilename, 'r') as infile:
+            to_parse = infile.read()
+    except FileNotFoundError:
+        if ignore_errs:
+            print("ERROR could not read file '%s'" % infilename)
+            sys.exit()
+        else:
+            raise
+except ValueError:
+    to_parse = sys.stdin.read()
 
 if not version:
     import json
-    json_in = json.loads(to_parse)
+    try:
+        json_in = json.loads(to_parse)
+    except:
+        if ignore_errs:
+            print("ERROR could not parse JSON in file '%s'" % infilename)
+            sys.exit()
+        else:
+            raise
     version = json_in['nbformat']
 
-json_in = reads(to_parse, version)
-
+try:
+    json_in = reads(to_parse, version)
+except:
+    if ignore_errs:
+        print("ERROR could not process ipynb JSON in file '%s'" % infilename)
+        sys.exit()
+    else:
+        raise
+        
 if hasattr(json_in, 'worksheets'):
     # IPython
     sheets = json_in.worksheets
@@ -70,4 +105,13 @@ for sheet in sheets:
 if 'signature' in json_in.metadata:
     json_in.metadata['signature'] = ""
 
-write(json_in, sys.stdout, version)
+try:
+    outfile_idx = sys.argv.index('--outfile')
+    outfilename = sys.argv[outfile_idx+1]
+    import os
+    outfilename = os.path.expanduser(outfilename)
+    outstream = open(outfilename, 'w')
+except ValueError:
+    outstream = sys.stdout
+    
+write(json_in, outstream, version)
